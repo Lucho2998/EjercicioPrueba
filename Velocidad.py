a2b271d6cb49c54c53ea519211586386076d85f5
Lucho2998 <46898175+Lucho2998@users.noreply.github.com>
2019-01-21 09:40:19 -0600

    Luis Rentería, grupo 02
    hola profe

---
+++ Velocidad.py
@@ -0,0 +1,15 @@
+# Autor: Luis Rentería
+# Calcula la velucidad
+
+t=int(input("inserte el tiempo de viaje en horas:"))
+d=int(input("inserte la distancia del viaje en kilometros:"))
+
+v=d/t
+
+print("La velocidad promedio es:",v,"km/h")
+
+
+
+
+
+
